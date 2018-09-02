781f7f09d2729a022e2e1b380b095f43e03ea0b3
imkimbosung <ghalsru@naver.com>
2018-09-03 00:25:13 +0900

    db update

---
+++ CITOS/db/db_info.js
@@ -0,0 +1,11 @@
+module.exports = (function () {
+  return {
+    local: {
+      host     : 'localhost',
+      user     : 'devel',
+      password : 'devel',
+      database : 'citos',
+      multipleStatements : true
+    }
+  }
+})();
---
+++ CITOS/routes/card.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var mysql_dbc = require('../db/db_con')();
+var connection = mysql_dbc.init();
+
+
+
+// /card
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+// 카드 정보 조회
+router.get('/:id', function(req,res,next){
+
+});
+
+// 카드 정보 등록
+router.post('/add', function(req,res,next){
+
+});
+
+// 카드 정보 삭제
+router.delete('/remove', function(req,res,next){
+
+});
+
+// 카드 정보 수정
+router.put('/edit', function(req, res, next){
+
+});
+
+
+module.exports = router;
