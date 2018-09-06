b9bc2e1a2c2a5d5a826f90651b00303a8de90031
imkimbosung <ghalsru@naver.com>
2018-09-06 22:51:03 +0900

    qrcode 정리

--- CITOS/funUsers/funUsers.js
+++ CITOS/funUsers/funUsers.js
@@ -42,23 +42,29 @@ fn.signup = function (req, res, next) {
 }
 
 fn.signin = function (req, res, next) {
-  var sql_select = 'SELECT * FROM customer_info WHERE id=?';
+  var sql_select = 'SELECT * FROM `customer_info` WHERE `id`= ? ' ;
 
   var
     user_id = req.body.userid,
     password = req.body.password;
 
   connection.query(sql_select, user_id, function(err, result) {
+    console.log(result);
     if (err) {
       console.log('err :' + err);
+      res.send('에러!');
     }else {
       if (result.length === 0) {
         res.json({success: false, msg: '해당 유저가 존재하지 않습니다.'})
+        console.log('아이디 오류!' );
       } else {
         if(!bcrypt.compareSync(password, result[0].pw)) {
+          console.log('비밀번호 오류');
           res.json({success: false, msg: '비밀번호가 일치하지 않습니다.'})
         } else {
-          res.json({success: true})
+          console.log('success login');
+          // res.json({success: true})
+          res.send('true');
         }
       }
     }
