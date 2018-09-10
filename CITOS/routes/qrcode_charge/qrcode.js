78279a377fdf53a80291c5a6788c85072d37bcdb
imkimbosung <ghalsru@naver.com>
2018-09-10 17:16:06 +0900

    check point 2

--- CITOS/routes/qrcode_charge/qrcode.js
+++ CITOS/routes/qrcode_charge/qrcode.js
@@ -1,25 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var QRCode = require('qrcode');
-var Image = require('canvas');
+var mkqr = require('../../funQrcode/funQrcode');
+// var Image = require('canvas');
 
 
-
-router.post('/', function (req, res) {
-    var
-      price = req.body.price,
-      userinfo = req.body.userinfo,
-      params = [price, userinfo];
-  QRCode.toDataURL(params, function(err, url){
-    if(err){
-      console.log(err);
-      res.send(err);
-    }else{      
-      console.log(url);
-      res.send(url);
-    }
-  });
-})
+router.post('/', function (req, res, next) {
+  mkqr.mkqrcode(req, res, next);
+});
 
 
 module.exports = router;
